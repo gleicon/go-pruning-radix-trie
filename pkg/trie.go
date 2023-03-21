e6756dbb7c5474870a5b7c9fe0d348a63eac0d7a
Gleicon Moraes <gleicon@gmail.com>
2023-03-20 22:45:26 -0300

    type structure

--- pkg/trie.go
+++ pkg/trie.go
@@ -1,7 +1,9 @@
 package go_pruning_trie
 
+type NodeList map[string]*Node
+
 type Node struct {
-	List []map[string]*Node
+	List NodeList
 	//Does this node represent the last character in a word?
 	//0: no word; >0: is word (termFrequencyCount)
 	TermFrequencyCount         uint64
@@ -24,6 +26,26 @@ func newPruningRadixTrie() (*PruningRadixTrie, error) {
 	rootNode, _ := newNode(0)
 	prt := PruningRadixTrie{TermCount: 0, TermCountLoaded: 0, trie: rootNode}
 	return &prt, nil
+}
+
+func (prt *PruningRadixTrie) AddTerm(term string, termFrequencyCount uint64) {
+	nodeList := NodeList{}
+	prt._add_term(prt.trie, term, termFrequencyCount, 0, 0, nodeList)
+}
+
+func (prt *PruningRadixTrie) UpdateMaxCounts(nodeList NodeList, termFrequencyCount uint64) {
+	// beware the elusive pointers
+	for key := range nodeList {
+		node := nodeList[key]
+		if termFrequencyCount > node.TermFrequencyCountChildMax {
+			node.TermFrequencyCountChildMax = termFrequencyCount
+		}
+	}
+}
+
+// (Node curr, String term, long termFrequencyCount, int id, int level, List<Node> nodeList)
+func (prt *PruningRadixTrie) _add_term(curr *Node, term string, termFrequencyCount uint64, id int, level int, nodeList NodeList) {
+
 }
 
 func PruningTrieFind(token string) string {
