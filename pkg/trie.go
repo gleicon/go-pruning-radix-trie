d737ba1a4bff52ad39ebe3812393ed850b8c8f26
Gleicon Moraes <gleicon@gmail.com>
2023-03-20 22:30:42 -0300

    data structures

--- pkg/trie.go
+++ pkg/trie.go
@@ -1,5 +1,31 @@
 package go_pruning_trie
 
+type Node struct {
+	List []map[string]*Node
+	//Does this node represent the last character in a word?
+	//0: no word; >0: is word (termFrequencyCount)
+	TermFrequencyCount         uint64
+	TermFrequencyCountChildMax uint64
+}
+
+func newNode(termFrequencyCount uint64) (*Node, error) {
+	node := Node{TermFrequencyCount: termFrequencyCount, TermFrequencyCountChildMax: 0}
+
+	return &node, nil
+}
+
+type PruningRadixTrie struct {
+	TermCount       uint64
+	TermCountLoaded uint64
+	trie            *Node
+}
+
+func newPruningRadixTrie() (*PruningRadixTrie, error) {
+	rootNode, _ := newNode(0)
+	prt := PruningRadixTrie{TermCount: 0, TermCountLoaded: 0, trie: rootNode}
+	return &prt, nil
+}
+
 func PruningTrieFind(token string) string {
 	return "lero"
 }
